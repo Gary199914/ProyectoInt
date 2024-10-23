e25ad09316c0972d9ed5f492e644afab5852f4b9
gary199914 <deymar553@gmail.com>
2024-10-23 15:05:57 -0400

    modificacion del archivo main para la funcionalidad de monitoreo de consumo (Precision de datos de consumo)

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -23,6 +23,11 @@ const int sensorPin = 35;
 
 unsigned long previousMillis = 0;
 const long interval = 5000;
+int mVperAmp = 66;
+int Watt = 0;
+double Voltage = 0;
+double VRMS = 0;
+double AmpsRMS = 0;
 
 void connectToWifi() {
   Serial.println("Conectando a la red WiFi...");
@@ -92,7 +97,35 @@ void onMqttPublish(uint16_t packetId) {
   Serial.print("  packetId: ");
   Serial.println(packetId);
 }
-
+float getVPP()
+{
+  float result;
+  int readValue;                
+  int maxValue = 0;             
+  int minValue = 4096;          
+  
+   uint32_t start_time = millis();
+   while((millis()-start_time) < 1000)
+   {
+       readValue = analogRead(sensorPin);
+       
+       if (readValue > maxValue) 
+       {
+           
+           maxValue = readValue;
+       }
+       if (readValue < minValue) 
+       {
+           
+           minValue = readValue;
+       }
+   }
+   
+   
+   result = ((maxValue - minValue) * 3.3)/4096.0;
+      
+   return result;
+ }
 void setup() {
   pinMode(relayPin, OUTPUT);
   digitalWrite(relayPin, HIGH);
@@ -112,3 +145,26 @@ void setup() {
   connectToWifi();
 }
 
+void loop() {
+   Voltage = getVPP();
+  VRMS = (Voltage/2.0) *0.707;
+  AmpsRMS = ((VRMS * 1000)/mVperAmp)-0.3;
+  potencia = (AmpsRMS*240/1.2);
+  potencia = analogRead(sensorPin);
+  potencia = map(potencia, 0, 4095, 0, 100);
+  unsigned long currentMillis = millis();
+  //if (valAnterior != potencia) {
+  if (currentMillis - previousMillis >= interval) {
+  previousMillis = currentMillis;
+    
+    //potencia = random(1, 100);
+    Serial.println();
+    Serial.printf("potencia = %.2f kW/h \n", potencia);
+    
+    uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_POT, 1, true, String(potencia).c_str());
+    Serial.printf("Publicando datos en el canal %s con QoS 1, packetId: %i ", MQTT_PUB_POT, packetIdPub1);
+    Serial.printf("Valor: %.2f \n", potencia);
+    valAnterior = potencia;
+    delay(250);
+  }
+}
